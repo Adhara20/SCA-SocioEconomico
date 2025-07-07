14eb2f4f3c9788be636164fcb024fa85ea09ac1c
Sergio <sergiotomascontrerasdiaz@gmail.com>
2025-07-07 10:56:38 -0700

    subiendo mis cambios

---
+++ src/clases/Estudiante.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package clases;
+/**
+ *
+ * @author Sergi
+ */
+public class Estudiante {
+    public int id, fkcarrera;
+    public String nombre, matricula, grupo, telefono;
+    public boolean estatus;
+}
